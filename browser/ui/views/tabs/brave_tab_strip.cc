cca0841ec382e0304f7d940f3b84e31919bddcb3
Claudio DeSouza <cdesouza@brave.com>
2026-01-26 13:58:20 +0000

    [cr146] `TabStrip::tab_container_` a pointer
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/71c287856f514829a6dcae14a4458d3ca5a1468f
    
    commit 71c287856f514829a6dcae14a4458d3ca5a1468f
    Author: Darryl James <dljames@chromium.org>
    Date:   Thu Jan 22 14:44:28 2026 -0800
    
        Reland "[Vertical Tabs] Lazily Load TabStrips when Switching Modes"
    
        This reverts commit 57de341fa7b9d85001358f9fb1ae80532b2754eb.
    
        Reason for revert: Guard guard root_node before use in VerticalTabStripRegionView::InitializeTabStrip and VerticalTabStripRegionView::ResetTabStrip
    
        Original change's description:
        > Revert "[Vertical Tabs] Lazily Load TabStrips when Switching Modes"
        >
        > This reverts commit 757777b28fc5e6c16fa45a253af222363919bdae.
        >
        > Reason for revert: breaks multiple Linux and Mac bots
        > First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/80596
        >
        > Original change's description:
        > > [Vertical Tabs] Lazily Load TabStrips when Switching Modes
        > >
        > > Change-Id: I2df5ca6b6d666a523834324b439cbb671bd69981
        > > Bug: 468100896
        > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7508566
        > > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        > > Commit-Queue: Darryl James <dljames@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#1572767}
        >
        > Bug: 468100896, 477782072
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Bug: 468100896
        > Change-Id: I6076211bd73fde7b00d40959bc47e0281b8f141d
        > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7510563
        > Owners-Override: Viktor Semeniuk <vsemeniuk@google.com>
        > Auto-Submit: Viktor Semeniuk <vsemeniuk@google.com>
        > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
        > Cr-Commit-Position: refs/heads/main@{#1572818}
    
        Bug: 468100896, 477782072
        Bug: 468100896
        Change-Id: Ifcef1ca9a70af8cbb44122aaedfae36a2e999ddf
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511474
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Commit-Queue: Darryl James <dljames@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1573324}

--- browser/ui/views/tabs/brave_tab_strip.cc
+++ browser/ui/views/tabs/brave_tab_strip.cc
@@ -263,7 +263,7 @@ void BraveTabStrip::OnAlwaysHideCloseButtonPrefChanged() {
 }
 
 TabContainer* BraveTabStrip::GetTabContainerForTesting() {
-  return &tab_container_.get();  // IN-TEST
+  return tab_container_.get();  // IN-TEST
 }
 
 bool BraveTabStrip::ShouldPaintTabAccent(const Tab* tab) const {
