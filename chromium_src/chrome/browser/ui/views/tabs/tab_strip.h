3c26dc139cb42c429427f077b063f67ac6dd6c04
Claudio DeSouza <cdesouza@brave.com>
2026-02-12 18:00:40 +0000

    [cr146][cr147] `TabStrip` ctor taking `TabHoverCardController`
    This change touches in several places where `TabHoverCardController`
    is being plumbed into, to make sure we are passing a
    `BraveTabStripRegionView`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/37fc5709e3536bdccf1ec86f7e358e08d7e3aeb7
    
    commit 37fc5709e3536bdccf1ec86f7e358e08d7e3aeb7
    Author: Dominic Austria <dominicaustria@google.com>
    Date:   Tue Feb 10 14:54:11 2026 -0800
    
        [Reland][Vertical Tabs] Render Vertical Tab Hovercards
    
        Basic rendering of hovercards on vertical tabs. Screencast link:
        https://screencast.googleplex.com/cast/NTA3OTUwMDEzOTI2NjA0OHxkYmQ1Y2M4Ny03MA
    
        Bug: 465159270
        Change-Id: I7fb43ddd6ee312d709de24e3170b7ffca593eb64
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7556332
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Commit-Queue: Dominic Austria <dominicaustria@google.com>
        Cr-Commit-Position: refs/heads/main@{#1582838}

--- chromium_src/chrome/browser/ui/views/tabs/tab_strip.h
+++ chromium_src/chrome/browser/ui/views/tabs/tab_strip.h
@@ -30,10 +30,6 @@ class BraveTabHoverCardController;
   bool ShouldPaintTabAccent(const Tab* tab) const override;                \
   std::optional<SkColor> GetTabAccentColor(const Tab* tab) const override; \
   ui::ImageModel GetTabAccentIcon(const Tab* tab) const override;          \
-  static constexpr bool IsUsingBraveTabHoverCardController() {             \
-    return std::is_same_v<std::unique_ptr<BraveTabHoverCardController>,    \
-                          decltype(TabStrip::hover_card_controller_)>;     \
-  }                                                                        \
   virtual TabDragContext* GetDragContext
 
 #define TabHoverCardController BraveTabHoverCardController
@@ -41,8 +37,5 @@ class BraveTabHoverCardController;
 #undef TabHoverCardController
 #undef GetDragContext
 #undef UpdateHoverCard
-
-static_assert(TabStrip::IsUsingBraveTabHoverCardController(),
-              "Should use BraveTabHoverCardController");
 
 #endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_UI_VIEWS_TABS_TAB_STRIP_H_
