282d067ae00e2ebcef724eb09e7737c1b084e0b6
Artem Samoilenko <artem@brave.com>
2026-02-18 17:23:21 -0500

    [cr147][Android] Changes for ToolbarSwipeLayout
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/6257cc0a04731f19de38049632765ee5ffa5f249
    
    commit 6257cc0a04731f19de38049632765ee5ffa5f249
    Author: Yanting Yu <yyanting@google.com>
    Date:   Tue Feb 17 13:22:09 2026 -0800
    
        Fix the toolbar swipe animation in NTP (Part2).
    
        This is the follow up CL for crrev.com/c/7552991. In this CL, we fixed
        1) toolbar jumps down when swiping the toolbar from newly created NTP;
        2) the empty bar is observed above the bottom toolbar after swipe to
        non-NTP tab from newly created NTP.
    
        Root cause: When preparing the swipe animation, we use the stale toolbar
        capture including the top padding for E2E in NTP. Even though the
        toolbar is notified to remove its own padding before swipe starts. We
        fixed this bug by forcing the toolbar to update its layout and capture
        the bitmap.
    
        Before: https://screencast.googleplex.com/cast/NTAyMDk1NTYwNzgyNjQzMnxkNWU5ZGQwYy1jMw
        After: https://screencast.googleplex.com/cast/NjI5MTcyNTEzMDY2MTg4OHxkMzE3Y2ZhNi1lYQ
    
        Bug: 469708907, 480857474
        Change-Id: I7a365b8f1a11ea65d7b3c13b54e00bfb7f1d88ce
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7563889
        Commit-Queue: Yanting Yu <yyanting@google.com>
        Reviewed-by: Patrick Noland <pnoland@chromium.org>
        Reviewed-by: Xi Han <hanxi@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1585966}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -2103,7 +2103,8 @@ public class BytecodeTest {
                         LayoutManager.class,
                         TopUiThemeColorProvider.class,
                         NonNullObservableSupplier.class,
-                        ViewGroup.class));
+                        ViewGroup.class,
+                        Runnable.class));
         Assert.assertTrue(
                 constructorsMatch(
                         "org/chromium/components/embedder_support/view/ContentView",
