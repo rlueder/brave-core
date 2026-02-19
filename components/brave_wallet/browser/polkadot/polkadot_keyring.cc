e9eec77fce7fb83ac1985a790ebc616e2b191221
Christian Mazakas <christian.mazakas@gmail.com>
2026-02-19 13:21:53 -0800

    update keyrings to use predicate for account whitelisting

--- components/brave_wallet/browser/polkadot/polkadot_keyring.cc
+++ components/brave_wallet/browser/polkadot/polkadot_keyring.cc
@@ -12,11 +12,6 @@
 #include "base/containers/span_writer.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
-#include "base/numerics/byte_conversions.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/strings/string_util.h"
-#include "brave/components/brave_wallet/browser/blockchain_utils.h"
-#include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
 #include "brave/components/brave_wallet/browser/internal/hd_key.h"
 #include "brave/components/brave_wallet/browser/scrypt_utils.h"
 #include "brave/components/brave_wallet/common/common_utils.h"
@@ -76,9 +71,11 @@ bool IsAllowedScryptParams(uint32_t n, uint32_t p, uint32_t r) {
 
 PolkadotKeyring::PolkadotKeyring(
     base::span<const uint8_t, kPolkadotSeedSize> seed,
-    mojom::KeyringId keyring_id)
+    mojom::KeyringId keyring_id,
+    base::RepeatingCallback<bool(const std::string&)> is_address_allowed)
     : root_account_key_(HDKeySr25519::GenerateFromSeed(seed)),
-      keyring_id_(keyring_id) {
+      keyring_id_(keyring_id),
+      is_address_allowed_(std::move(is_address_allowed)) {
   // Can be useful to remember:
   // https://wiki.polkadot.com/learn/learn-account-advanced/#derivation-paths
 
@@ -150,7 +147,7 @@ HDKeySr25519& PolkadotKeyring::EnsureKeyPair(uint32_t account_index) {
 
 std::optional<std::string> PolkadotKeyring::AddNewHDAccount(uint32_t index) {
   auto addr = GetAddress(index, IsTestnet() ? kWestendPrefix : kPolkadotPrefix);
-  if (IsOfacAddress(addr)) {
+  if (!is_address_allowed_.Run(addr)) {
     return std::nullopt;
   }
   return addr;
