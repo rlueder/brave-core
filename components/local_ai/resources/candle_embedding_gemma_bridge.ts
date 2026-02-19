left-TTC/kilo-core

Add WASM resources for candle embedding gemma
Add the TypeScript bridge, HTML page, and build targets for the
candle embedding gemma WASM module that runs in an untrusted
WebUI context.

components/local_ai/resources/candle_embedding_gemma_bridge.ts
@@ -1,0 +1,50 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import {
+  CandleService,
+  EmbeddingGemmaInterfaceReceiver,
+} from 'gen/brave/components/local_ai/common/candle.mojom.m.js'
+
+console.log('[Candle WASM] Embedding Gemma Script loaded')
+
+console.log('[Candle WASM] Initializing Mojo connection...')
+
+// Initialize connection to the browser-side CandleService
+const candleService = CandleService.getRemote()
+
+console.log('[Candle WASM] CandleService remote obtained:', candleService)
+
+// Implement the EmbeddingGemmaInterface Mojo observer
+class EmbeddingGemmaInterfaceImpl {
+  receiver: EmbeddingGemmaInterfaceReceiver
+
+  constructor() {
+    this.receiver = new EmbeddingGemmaInterfaceReceiver(this)
+  }
+
+  // Implementation of EmbeddingGemmaInterface::Embed
+  async embed(input: string): Promise<{ output: number[] }> {
+    console.log('[Candle WASM] Embed called (model not loaded yet):', input)
+    // Model loading will be added in branch 3
+    return { output: [] }
+  }
+
+  getPendingRemote() {
+    return this.receiver.$.bindNewPipeAndPassRemote()
+  }
+}
+
+// Create and register the EmbeddingGemmaInterface implementation
+console.log(
+  '[Candle WASM] Creating EmbeddingGemmaInterface ' + 'implementation...',
+)
+const embeddingGemmaImpl = new EmbeddingGemmaInterfaceImpl()
+console.log(
+  '[Candle WASM] Binding EmbeddingGemmaInterface ' + 'to CandleService...',
+)
+candleService.bindEmbeddingGemma(embeddingGemmaImpl.getPendingRemote())
+
+console.log('[Candle WASM] Embedding Gemma WASM bridge initialized!')

rlueder/brave-core

components/local_ai/resources/candle_embedding_gemma_bridge.ts
@@ -1,0 +1,50 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import {
+  LocalAIService,
+  EmbeddingGemmaInterfaceReceiver,
+} from 'gen/brave/components/local_ai/common/local_ai.mojom.m.js'
+
+console.log('[Candle WASM] Embedding Gemma Script loaded')
+
+console.log('[Candle WASM] Initializing Mojo connection...')
+
+// Initialize connection to the browser-side LocalAIService
+const localAIService = LocalAIService.getRemote()
+
+console.log('[Candle WASM] LocalAIService remote obtained:', localAIService)
+
+// Implement the EmbeddingGemmaInterface Mojo observer
+class EmbeddingGemmaInterfaceImpl {
+  receiver: EmbeddingGemmaInterfaceReceiver
+
+  constructor() {
+    this.receiver = new EmbeddingGemmaInterfaceReceiver(this)
+  }
+
+  // Implementation of EmbeddingGemmaInterface::Embed
+  async embed(input: string): Promise<{ output: number[] }> {
+    console.log('[Candle WASM] Embed called (model not loaded yet):', input)
+    // Model loading will be added in branch 3
+    return { output: [] }
+  }
+
+  getPendingRemote() {
+    return this.receiver.$.bindNewPipeAndPassRemote()
+  }
+}
+
+// Create and register the EmbeddingGemmaInterface implementation
+console.log(
+  '[Candle WASM] Creating EmbeddingGemmaInterface ' + 'implementation...',
+)
+const embeddingGemmaImpl = new EmbeddingGemmaInterfaceImpl()
+console.log(
+  '[Candle WASM] Binding EmbeddingGemmaInterface ' + 'to LocalAIService...',
+)
+localAIService.bindEmbeddingGemma(embeddingGemmaImpl.getPendingRemote())
+
+console.log('[Candle WASM] Embedding Gemma WASM bridge initialized!')